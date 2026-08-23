92e445c825faf1f902f99839ea45ce99f9cc2991
Kane-kiraa <your-github-email@example.com>
2026-08-23 21:49:25 +0700

    Keep only frontend

--- backend/src/main/java/com/flamecrust/api/product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flamecrust.api.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByActiveTrueOrderByIdAsc();
-    List<Product> findByCategoryIgnoreCase(String category);
-}
