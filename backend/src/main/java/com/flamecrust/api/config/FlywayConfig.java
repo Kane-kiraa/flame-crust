d011652942ec25017ed2a714d5ef59ecfa5eb94a
Kane-kiraa <your-github-email@example.com>
2026-08-28 11:52:10 +0700

    Allow toggling Flyway with FLYWAY_ENABLED env variable

--- backend/src/main/java/com/flamecrust/api/config/FlywayConfig.java
+++ backend/src/main/java/com/flamecrust/api/config/FlywayConfig.java
@@ -1,10 +1,12 @@
 package com.flamecrust.api.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.flyway.enabled", havingValue = "true", matchIfMissing = true)
 public class FlywayConfig {
 
     @Bean
